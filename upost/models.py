4cce2cd6c8f1a68a257981269a6f463fa28f6677
Ansu Kumari <ansu.kumari@outlook.com>
2018-04-05 22:05:36 +0530

    Make initial upost application with post and comment features and user authentication.

---
+++ upost/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
---
+++ upost/views.py
@@ -0,0 +1,84 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+def post_list(request):
+    posts = Post.objects.order_by('-created')	#To fetch data in reverse order of created
+    return render(request, 'upost/post_list.html', {'posts': posts}) 
+
+@login_required
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+		    post = form.save(commit=False)
+		    post.user = request.user
+		    post.save()
+		    return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+		return render(request, 'upost/post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'upost/post_detail.html', {'post': post})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False) #Table not changed yet
+            if post.user == request.user:
+                post.save() # since commit = True, changes saved to table
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'upost/post_edit.html', {'form': form})
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.user == request.user:
+        post.delete()
+    return redirect('post_list')
+
+@login_required
+def comment_add(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'upost/comment_add.html', {'form': form})
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if comment.post.user == request.user or comment.user == request.user:
+        comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
